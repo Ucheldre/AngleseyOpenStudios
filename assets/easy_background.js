6e7b2ec6a52655bdaed25ad633c1048009495aaf
HBIDamian <hbidamian@gmail.com>
2025-03-29 23:38:08 +0000

    Attempt 1 at fixing background transition

--- assets/easy_background.js
+++ assets/easy_background.js
@@ -1,24 +1,57 @@
-    function easy_background(e, t) {
-        var n = document.createElement("div");
-        n.innerHTML = " ", n.classList.add("easy_slider"), document.body.insertBefore(n, document.body.firstChild), t.slide.forEach(function(e, n) {
-            var o;
-            e && (document.querySelector(".easy_slider").innerHTML += (o = e) ? "<img src='" + o + "'>" : "", void 0 !== t.delay[n] && "" != typeof t.delay[n] && 0 != t.delay[n] || (t.delay[n] = 3e3))
-        }), document.querySelector(".easy_slider").style.display = "none", document.querySelector(e).style.backgroundSize = "cover", document.querySelector(e).style.backgroundRepeat = "no-repeat", document.querySelector(e).style.backgroundPosition = "center center", setTimeout(function() {
-            void 0 === t.transition_timing && (t.transition_timing = "ease-in"), void 0 === t.transition_duration && (t.transition_duration = 500);
-            var n = "all " + t.transition_duration + "ms " + t.transition_timing;
-            document.querySelector(e).style.WebkitTransition = n, document.querySelector(e).style.MozTransition = n, document.querySelector(e).style.MsTransition = n, document.querySelector(e).style.OTransition = n, document.querySelector(e).style.transition = n
-        }, 100);
-        var o = 1,
-            r = 0;
+function easy_background(e, t) {
+	// Create a container for the background layers
+	var container = document.createElement("div");
+	container.classList.add("easy_background_container");
+	container.style.position = "fixed";
+	container.style.top = 0;
+	container.style.left = 0;
+	container.style.width = "100%";
+	container.style.height = "100%";
+	container.style.zIndex = -1;
+	document.body.insertBefore(container, document.body.firstChild);
 
-        function i() {
-            t.slide.forEach(function(n, i) {
-                o > 1 ? (setTimeout(function() {
-                    document.querySelector(e).style.backgroundImage = "url('" + n + "')"
-                }, r), r += t.delay[i]) : (o++, r = t.delay[i], document.querySelector(e).style.backgroundImage = "url('" + n + "')")
-            })
-        }
-        i(), setInterval(function() {
-            i()
-        }, t.delay.length)
-    }
+	// Create two layered divs to allow cross-fading
+	var layer1 = document.createElement("div");
+	var layer2 = document.createElement("div");
+	[layer1, layer2].forEach(function(layer) {
+		layer.style.position = "absolute";
+		layer.style.top = 0;
+		layer.style.left = 0;
+		layer.style.right = 0;
+		layer.style.bottom = 0;
+		layer.style.backgroundSize = "cover";
+		layer.style.backgroundRepeat = "no-repeat";
+		layer.style.backgroundPosition = "center center";
+		layer.style.transition = "opacity " + (t.transitionDuration || 1000) + "ms ease-in-out";
+		layer.style.opacity = 0;
+		container.appendChild(layer);
+	});
+	
+	// Show the first slide immediately
+	layer1.style.opacity = 1;
+	layer1.style.backgroundImage = "url('" + t.slide[0] + "')";
+	
+	var index = 1;
+	var currentLayer = layer1;
+	var nextLayer = layer2;
+	
+	function changeBackground() {
+		// Set next slide image and fade it in
+		nextLayer.style.backgroundImage = "url('" + t.slide[index] + "')";
+		nextLayer.style.opacity = 1;
+		
+		// After the transition, fade out the current layer
+		setTimeout(function() {
+			currentLayer.style.opacity = 0;
+			// Swap layers for the next iteration
+			var temp = currentLayer;
+			currentLayer = nextLayer;
+			nextLayer = temp;
+			// Update index
+			index = (index + 1) % t.slide.length;
+		}, 50);
+	}
+	
+	// Change background every delay period (using the first delay for all slides)
+	setInterval(changeBackground, t.delay[0]);
+}
